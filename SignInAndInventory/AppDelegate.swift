c3fdcc29562263f6264566d2f665e0c0d6edb594
Miguel Aguila <maguila.apple@gmail.com>
2023-02-04 09:52:32 +0800

    Added Firebase

--- SignInAndInventory/AppDelegate.swift
+++ SignInAndInventory/AppDelegate.swift
@@ -5,15 +5,17 @@
 //  Created by OPSolutions on 2/3/23.
 //
 
+import Firebase
 import UIKit
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         return true
     }
 
